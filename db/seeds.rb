b4239c773f11299c0de719445eb83948332561f7
nakamura shinichirou <naka5313@gmail.com>
2013-12-17 20:04:34 +0900

    update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#coding: utf-8
 
 Event.create(title: '忘年会', description: '会社の忘年会をします')
 Event.create(title: 'クリスマス会', description: '家族でクリスマス会をします。')
