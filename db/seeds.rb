d5445d68770f25e1c0e8a8b8a13089b5a8ab4979
nakamura shinichirou <naka5313@gmail.com>
2013-12-17 20:06:42 +0900

    テストデータの文言修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,4 @@
 
 Event.create(title: '忘年会', description: '会社の忘年会をします')
 Event.create(title: 'クリスマス会', description: '家族でクリスマス会をします。')
-Event.create(title: '子供と出かける日', description: '子供を連れて、買い物にきます')
+Event.create(title: '子供と出かける日', description: '子供を連れて、買い物に行きます')
