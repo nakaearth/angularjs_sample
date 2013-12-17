1ac0d7db1acd59e6046ac9146145825bdfd79a65
nakamura shinichirou <naka5313@gmail.com>
2013-12-17 12:22:24 +0900

    update files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,6 +9,7 @@
 //
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
+//= require angular
+//= require angular-resource
 //= require app
-//= angular
 //= require_tree ./angular
